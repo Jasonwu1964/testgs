4c95302ea61697fba6e4381f2dc69837821eb959
Jasonwu1964 <69262948+Jasonwu1964@users.noreply.github.com>
2021-03-01 15:30:32 -0800

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,7 @@
 function testGS(){
     
     const url="https://script.google.com/macros/s/AKfycbwye4OYprVe-DNPY_A40VtkIJ-dYps3rYGa-nqZEqGfHBnzrOg7JLky/exec"
+
     fetch(url)
       .then(d => d.json())
       .then(d => {
@@ -10,3 +11,24 @@ function testGS(){
 }
 
 document.getElementById("btn").addEventListener("click",testGS);
+
+
+function addGS(){
+    
+    const url="https://script.google.com/macros/s/AKfycbwye4OYprVe-DNPY_A40VtkIJ-dYps3rYGa-nqZEqGfHBnzrOg7JLky/exec"
+
+    fetch(url,{
+      method: 'POST',
+      mode: 'no-cors',
+      cache: 'no-cache',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      body: JSON.stringify({name:"Jason"})
+    });
+
+}
+
+document.getElementById("btn").addEventListener("click",testGS);
+document.getElementById("btn2").addEventListener("click",addGS);
